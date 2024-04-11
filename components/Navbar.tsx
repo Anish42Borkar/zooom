9327c3afb75d7c2fdfc80ce789a353c4a17c1e5b
anish42borkar <anishborkar42@gmail.com>
2024-04-11 13:05:30 +0530

    added sidebar and navbar with routhing

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import MobileNav from "./MobileNav";
+
+const Navbar = () => {
+  return (
+    <nav className="flex-between fixed z-50 w-full bg-dark-1 px-6 py-4 lh:px-10">
+      <div className="flex items-center gap-1">
+        <Image
+          src="/icons/logo.svg"
+          height={32}
+          width={32}
+          alt="Zoom logo"
+          className="max-sm:size-10"
+        />
+        <p className="text-[26px] font-extrabold text-white max-sm:hid">
+          Zooom
+        </p>
+      </div>
+
+      <div className="flex-between gap-5">
+        {/* {cleark} */}
+        <MobileNav />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    lable: "Home",
+    route: "/",
+    imagUrl: "/icons/Home.svg",
+  },
+  {
+    lable: "Upcoming",
+    route: "/upcoming",
+    imagUrl: "/icons/Upcoming.svg",
+  },
+  {
+    lable: "Previous",
+    route: "/previous",
+    imagUrl: "/icons/Previous.svg",
+  },
+  {
+    lable: "Recordings",
+    route: "/recordings",
+    imagUrl: "/icons/Video.svg",
+  },
+  {
+    lable: "Personal Room",
+    route: "/personal-room",
+    imagUrl: "/icons/add-personal.svg",
+  },
+];
